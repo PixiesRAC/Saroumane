2395364b6615a8a3d25040c3484965113383b2c4
pixiesRAC <pixiesRAC@github.com>
2017-12-18 15:59:16 +0100

    First commit, Initialisation of CMake / and unit test, Adding LogHandler

---
+++ include/log/LogHandler.h
@@ -0,0 +1,73 @@
+
+#pragma once
+
+#include <fstream>
+
+#define LOG(level, msg)	LOG##level(msg)
+
+#if LOG_UNIT_TEST == 0
+
+#define LOGINFO(msg) RAClog::FileLogHandler::oFileStream << "[ INFO ] " << " [msg]: " <<  msg << "\n"; 
+
+#define LOGWARN(msg) RAClog::FileLogHandler::oFileStream << "[ WARN ] " << __FUNCTION__ << " [msg]: " <<  msg << "\n";
+
+#define LOGDEBUG(msg)
+
+#define LOGERROR(msg)
+
+#else
+
+#define LOGINFO(msg) "[ INFO ] "
+
+#define LOGWARN(msg) "[ WARN ] "
+
+#define LOGDEBUG(msg) "[ DEBUG ] "
+
+#define LOGERROR(msg)
+
+#endif
+
+
+namespace RAClog
+{
+
+    class	FileLogHandler
+    {
+
+	public:
+
+	    void			CreateFileLog(const std::string& fileSuffix);
+	    void			CloseFileLog();
+
+
+	    static FileLogHandler	*CreateInstance()
+	    {
+		if (!pInstance)
+		{
+		    pInstance = new FileLogHandler;
+		}
+	    }
+
+	    static void		DeleteInstance()
+	    {
+		if (pInstance)
+		{
+		    if (oFileStream.is_open())
+		    {
+			LOG(WARN, "You should Close the Log File with the function CloseFileLog before");
+			return ;
+		    }
+		    delete pInstance;	
+		}
+	    }
+
+	private :
+
+	    static FileLogHandler	*pInstance;
+
+	public:
+
+	    static std::ofstream    	oFileStream;
+
+    };
+}
